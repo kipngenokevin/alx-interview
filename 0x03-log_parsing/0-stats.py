72c59c630eb7b2c315a00f4d8e4291d9a4ccec47
kipngeno <kipngenokevin254@gmail.com>
2024-05-15 15:03:20 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,32 +1,50 @@
 #!/usr/bin/python3
-"""This module reads stdin line by line and executes metrics"""
+"""
+log parsing
+"""
+
 import sys
 import re
-pattern = (r'^(\d+\.\d+\.\d+\.\d+) - \[([^\]]+)\] "GET /projects/260 '
-           r'HTTP/1.1" (\d+) (\d+)$')
-i = 0
-total_size = 0
-status_codes = {"200": 0, "301": 0, "400": 0,
-                "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-try:
-    for line in sys.stdin:
-        match = re.match(pattern, line)
-        if match:
-            ip_address, date, status_code, file_size = match.groups()
-            file_size = int(file_size)
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-            i += 1
-            total_size += file_size
-            if i % 10 == 0:
-                print("File size: {}".format(total_size))
-                for code in sorted(status_codes.keys()):
-                    if status_codes[code] > 0:
-                        print("{} : {}".format(code, status_codes[code]))
-                        status_codes[code] = 0
-except KeyboardInterrupt:
-    print("\nFile Size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{} : {}".format(code, status_codes[code]))
-    sys.exit(1)
+
+
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
+
+
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
+
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
+
+                # File size
+                log["file_size"] += file_size
+
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
